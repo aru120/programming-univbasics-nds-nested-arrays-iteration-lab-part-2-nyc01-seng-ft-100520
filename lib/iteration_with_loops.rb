d72e1a269eb647e385217cc079cc42f381f24faa
Kevin Huang <khuang159@gmail.com>
2020-09-14 22:40:42 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,28 +1,12 @@
-src = [[1,4,2,5,3],[2,3,1,2]]
 
 def find_min_in_nested_arrays(src)
 
-newArr = []
-
-i = 0
-while i < src.count do
-  j = 0
-  
-  while j < src[i].count do
-    low = src[i][j]
-    if src[i][j] < low
-      low = src[i][j]
+  newArr = []
+  i = 0 
+    while  i < src.count do
+      sorted = src[i].sort
+      
     end
-    j+=1
-  end
-  puts j
-  newArr << low
-  i+=1
-end
-newArr
+  
 
 end
-
-find_min_in_nested_arrays(src)
-
-
